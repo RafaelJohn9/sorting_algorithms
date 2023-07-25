d5fdc31d08d9e00b1576fcbbc7d93f617f9e99a6
RafaelJohn9 <rafaeljohb@gmail.com>
2023-07-25 10:09:28 +0300

    quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,79 +1,70 @@
-#include <stdio.h>nmz
 #include "sort.h"
 
-/**
- * swap - Function to swap two integers
- * @a: first num
- * @b: second num
- **/
-void swap(int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
+int *originalArray;
+size_t originalSize;
+void print_it(void);
 
 /**
- * lomuto_partition- scheme of lomuto partition
- * @array: array of int
- * @low: low
- * @high: high
- * @size: size of array
- * Return: the partition
+ * quick_sort_helper- sorts an array of integers in ascending order using the algorithm
+ * @array:to  be sorted
+ * @size:size of the array
+ * @originalSize: original size of the array
  */
-int lomuto_partition(int *array, int low, int high, size_t size)
+void quick_sort_helper(int *array, size_t size)
 {
-	int pivot = array[high];
-	int lastNumIndex = high;
-	int j;
+	int *left, *right;
+	int i, lastNumIndex = (int)size - 1, temp, pivot, low;
+	size_t sizeOfRight,  sizeOfLeft;
 
-	for (j = low; j <= high - 1; j++)
-	{
-		if (array[j] > pivot && j < lastNumIndex)
-		{
-			swap(&array[lastNumIndex], &array[j]);
-			lastNumIndex = j;
-			print_array(array, size);
-		}
-		else if (array[j] <= pivot && j > lastNumIndex)
-		{
-			swap(&array[lastNumIndex], &array[j]);
-			lastNumIndex = j;
-			print_array(array, size);
-		}
-	}
-	return (lastNumIndex);
-}
-
-/**
- * quick_sort_helper- recursive function
- * @array:array of int
- * @low: low
- * @high: high
- * @size: size of array
- */
-void quick_sort_helper(int *array, int low, int high, size_t size)
-{
-	if (low < high)
-	{
-		int pivot_idx = lomuto_partition(array, low, high, size);
-
-		quick_sort_helper(array, low, pivot_idx - 1, size);
-		quick_sort_helper(array, pivot_idx + 1, high, size);
-	}
-}
-
-/**
- * quick_sort-function that sorts using the algo
- * @array: the array of int
- * @size: size of array
- */
-void quick_sort(int *array, size_t size)
-{
-	if (array == NULL || size < 2)
+	if (!array || size  == 1 || size == 0)
 	{
 		return;
 	}
+	pivot = array[lastNumIndex];
+	low = 0;
 
-	quick_sort_helper(array, 0, size - 1, size);
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] >  pivot && i < lastNumIndex)
+		{
+			temp = array[lastNumIndex];
+			array[lastNumIndex] = array[i];
+			array[i] = temp;
+			lastNumIndex = i;
+			print_it();
+		}
+		else if (array[i] <=  array[lastNumIndex] && i > lastNumIndex)
+		{
+			temp = array[lastNumIndex];
+			array[lastNumIndex] = array[i];
+			array[i] = temp;
+			lastNumIndex = i;
+			i = low - 1;
+			print_it();
+
+		}
+	}
+	left = array;
+	sizeOfLeft = lastNumIndex;
+	right = &array[lastNumIndex + 1];
+	sizeOfRight = size - (lastNumIndex + 1);
+	quick_sort_helper(left, sizeOfLeft);
+	quick_sort_helper(right, sizeOfRight);
+}
+
+void print_it(void)
+{
+	print_array(originalArray, originalSize);
+}
+	
+/**
+ * quick_sort- sorts an array of integers in ascending order using the algorithm
+ * @array:to  be sorted
+ * @size:size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	originalArray = array;
+	originalSize = size;
+	quick_sort_helper(array, size);
 }
--- bad/3-quick_sort.c
+++ bad/3-quick_sort.c
@@ -1,14 +1,17 @@
 #include "sort.h"
 
+int *originalArray;
+size_t originalSize;
+void print_it(void);
+
 /**
  * quick_sort_helper- sorts an array of integers in ascending order using the algorithm
  * @array:to  be sorted
  * @size:size of the array
  * @originalSize: original size of the array
  */
-void quick_sort_helper(int *array, size_t size, size_t originalSize)
+void quick_sort_helper(int *array, size_t size)
 {
-	static size_t sizeOfOriginal;
 	int *left, *right;
 	int i, lastNumIndex = (int)size - 1, temp, pivot, low;
 	size_t sizeOfRight,  sizeOfLeft;
@@ -19,7 +22,6 @@ void quick_sort_helper(int *array, size_t size, size_t originalSize)
 	}
 	pivot = array[lastNumIndex];
 	low = 0;
-	sizeOfOriginal = originalSize;
 
 	for (i = 0; i < (int)size; i++)
 	{
@@ -29,7 +31,7 @@ void quick_sort_helper(int *array, size_t size, size_t originalSize)
 			array[lastNumIndex] = array[i];
 			array[i] = temp;
 			lastNumIndex = i;
-			print_array(array, sizeOfOriginal);
+			print_it();
 		}
 		else if (array[i] <=  array[lastNumIndex] && i > lastNumIndex)
 		{
@@ -38,19 +40,23 @@ void quick_sort_helper(int *array, size_t size, size_t originalSize)
 			array[i] = temp;
 			lastNumIndex = i;
 			i = low - 1;
+			print_it();
 
-			print_array(array, sizeOfOriginal);
 		}
 	}
 	left = array;
 	sizeOfLeft = lastNumIndex;
 	right = &array[lastNumIndex + 1];
 	sizeOfRight = size - (lastNumIndex + 1);
-	quick_sort(left, sizeOfLeft);
-	quick_sort(right, sizeOfRight);
+	quick_sort_helper(left, sizeOfLeft);
+	quick_sort_helper(right, sizeOfRight);
 }
 
-
+void print_it(void)
+{
+	print_array(originalArray, originalSize);
+}
+	
 /**
  * quick_sort- sorts an array of integers in ascending order using the algorithm
  * @array:to  be sorted
@@ -58,5 +64,7 @@ void quick_sort_helper(int *array, size_t size, size_t originalSize)
  */
 void quick_sort(int *array, size_t size)
 {
-	 quick_sort_helper(array, size, size);
+	originalArray = array;
+	originalSize = size;
+	quick_sort_helper(array, size);
 }
